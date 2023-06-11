f3d9a36fd50664108bb5a1cbd445c60f6b64a6c8
shanmugapriyamuthuraman <priyamuthuraman2002@gmail.com>
2023-06-11 21:53:20 +0530

    source

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import './App.css';
+import question from "./question";
+
+function App() {
+  const [questionIdx, setQuestionIdx]=useState(0);
+  const [score, setScore]=useState(0);
+  const [showScore,setShowScore]=useState(false);
+  const currQuestion=question[questionIdx]
+  
+  const   selectOption=(idx)=>{
+    if(currQuestion.answer === idx){
+      setScore(score+1);
+    }
+    const nextQ =questionIdx+1;
+    if(nextQ<question.length){
+      setQuestionIdx(questionIdx +1);
+    }
+    else{
+      setShowScore(true);
+    }
+  }; 
+  const reset=()=>{
+    setQuestionIdx(0);
+    setScore(0);
+    setShowScore(false);
+  }
+  return (
+    <div className="quiz-container">
+        {
+          showScore?( 
+            <>
+            <div className='btn'>
+          <h1> Your score is :{score}</h1>
+            <button onClick={reset}>Restart Quiz</button>
+            </div>
+            </>
+          ):(
+            <div className='quiz-container__question'>
+            <p>{currQuestion.question}</p>
+            <div className='quiz-container__options'>
+              <ul className='quiz-container__ul'>
+              {currQuestion.options.map((option,i)=>{
+                  return <li className='quiz-container__li' onClick={()=>selectOption(i)}>{option}</li>
+              })}
+                
+                
+              </ul>
+            </div>
+          </div>
+          )
+        }
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/question.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        question:"National Animal ?",
+        options:["Lion", "Tiger", "Dog", "Dear"],
+        answer:1,
+    },
+    {
+        question:"Fastest shorthand writer was",
+        options:[ "J.R.D. Tata", "J.M. Tagore","Dr. G. D. Bist", "Khudada Khan"],
+        answer:2,
+    },
+    {
+        question:"How many days are there in a week?",
+        options:["4", "5", "7", "6"],
+        answer:2,
+    },
+    {
+        question:"National Brid",
+        options:["Crow", "Hen", "Parrot", "Peacock"],
+        answer:3,
+    },
+]
